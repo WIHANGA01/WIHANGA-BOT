bb9ad92ccbf6094e1edb3fa2445fbc52da5efcfb
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-11-30 18:15:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "2NEkDSTK#P2hnqjY2nZVHFIQYylCYmSIqQeqG6Oaxr9i22tDuPIU",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=P7Y2CbzA#Ew2WIpi8GhdDuzc0ZyOaJ6OpJe-AyEI8Cc9pBM-fgz0",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
